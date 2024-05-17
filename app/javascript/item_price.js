08c95c3c9b7b73f5f8430ce40dcd9b987d0a9a4b
310Wakana <wakana25.aaa@gmail.com>
2024-05-17 21:50:16 +0900

    fourth commit

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,7 @@ window.addEventListener('turbo:load', () => {
     const priceInput = document.getElementById("item-price")
     priceInput.addEventListener("input", () => {
         const inputValue = priceInput.value;
-        console.log(inputValue);
+
        
         const addTaxDom = document.getElementById("add-tax-price");
         const tax = Math.floor(inputValue * 0.1);
