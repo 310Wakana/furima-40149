e982182e597474d1c5f63ba83b7357fa8c41a17e
310Wakana <wakana25.aaa@gmail.com>
2024-05-24 17:21:42 +0900

    seventh commit

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,11 +1,11 @@
 class RecordAddress
     include ActiveModel::Model
-    attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building, :phone_number, :record, :price, :user_id, :token, :item_id
+    attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building, :phone_number, :record_id, :price, :user_id, :token, :item_id
 
     with_options presence: true do
-    validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid."}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid."}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :municipality 
+    validates :municipality
     validates :house_number
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid."}
     validates :token
@@ -17,6 +17,6 @@ class RecordAddress
 
     def save
         @record = Record.create(item_id: item_id, user_id: user_id)
-        @address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building: building, phone_number: phone_number, record_id: record_id)
+        @address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building: building, phone_number: phone_number, record_id: @record.id)
     end
 end
